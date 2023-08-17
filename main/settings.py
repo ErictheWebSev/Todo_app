5411768fe8bfd27b5f79e486828578ec2a95c0e7
Eric Alfred <alfredoeric@gmail.com>
2023-08-17 02:52:20 +0100

    comit

--- main/settings.py
+++ main/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
@@ -121,10 +122,6 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 
 # Default primary key field type
--- static/main.js
+++
@@ -1,6 +0,0 @@
-
-function openModal(modalId) {
-    let modal = document.getElementById(modalId);
-    modal.classList.add('left');
-    modal.style.left = '0';
-}
